e6573e89f24af65d4b901a2c9d94a44abc9f223f
Daniel <dan.gillet737@gmail.com>
2018-03-16 11:00:54 +0100

    Rename css override file

--- doc/conf.py
+++ doc/conf.py
@@ -101,7 +101,7 @@ html_static_path = ['_static']
 
 html_context = {
     'css_files': [
-        '_static/theme_overrides.css',  # override wide tables in RTD theme
+        '_static/theme_overrides_rtd.css',  # override wide tables in RTD theme
         ],
      }
 
